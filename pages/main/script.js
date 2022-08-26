a8f411ebb9bcf30854f5099128ccf87556997340
Aliaksey-Iliukevich <ilykevich.aleksey@gmail.com>
2022-08-26 14:27:31 +0300

    fix: style

--- pages/main/script.js
+++ pages/main/script.js
@@ -110,3 +110,23 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 
+  const s_swiper = new Swiper('.swiper', {
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
